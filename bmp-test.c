215f4dc46fffeff0d5297966f35e5f8e5cb44253
Atharva Lele <itsatharva@gmail.com>
2020-11-03 17:10:17 +0000

    bmp280-test: add skeleton testing file
    Signed-off-by: Atharva Lele <itsatharva@gmail.com>

---
+++ bmp-test.c
@@ -0,0 +1,17 @@
+/*
+ * BMP280 Driver Testing File
+ *
+ * This file is used to test the working of the functions defined
+ * in the actual BMP280 library files - bmp280.c & bmp280.h
+ */
+
+#include <stdio.h>
+
+#include "bmp280.h"
+
+int main(int argc, char**argv)
+{
+    printf("BMP280 Testing\n");
+
+    return 0;
+}
